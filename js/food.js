232e858b5097fd92694a983a4ed5e6340dccbc59
Kweku Kwabi <kwaxe@yahoo.com>
2020-03-13 14:46:21 -0500

    Update food.js

--- js/food.js
+++ js/food.js
@@ -1,13 +1,59 @@
 function GetFood()
 {
-   var foodArray = ["pizza","apple","orange","burrito",
-   					"cake","soda","poptarts","guacamole",
-   					"taco","hamburger","casserole","pickle",
-   					"salad","mashed potatoes", "corn", "meatloaf",
-   					"Gyro","roast beef sandwich", "egg rolls",
-   					"fresh fruit","apple pie", "spaghetti", "lo mein",
-   					"Pho Dac Beit", "Jollof Rice", "Stir Fry", "Pad Thai" 
-   					];
+//    var foodArray = ["pizza","apple","orange","burrito",
+//    					"cake","soda","poptarts","guacamole",
+//    					"taco","hamburger","casserole","pickle",
+//    					"salad","mashed potatoes", "corn", "meatloaf",
+//    					"Gyro","roast beef sandwich", "egg rolls",
+//    					"fresh fruit","apple pie", "spaghetti", "lo mein",
+//    					"Pho Dac Beit", "Jollof Rice", "Stir Fry", "Pad Thai" 
+//    					];
+	
+	var foodArray = ["Brickson, Margaret A"
+"Loos, Jessica L"
+"Macgregor, Stephen J"
+"Fischer, Melinda C"
+"VanHuysse, Christopher S"
+"Blackwell, Nafeesa D"
+"Reid, Donnette L"
+"Dunlap, Laura M"
+"Villalovos, Lawrence D"
+"Lorence, Margaret M"
+"Nichols, Megan K",
+"Anderson, Nathan C",
+"Neale, Patty J",
+"Anderson, Robert F",
+"Musallam, Samir",
+"Russell, Sara J",
+"Huynh, Vi M",
+"Sunde, Tyrone J",
+"Lowe, Pamela R",
+"Childers, Melissa",
+"Moore, Brian P",
+"Maya, Ana L",
+"Glendening, Christopher J",
+"Mishica, Nathaniel J",
+"Hinson, Wallace E",
+"Xiong, Yia",
+"Le, Sarina E",
+"Stout, Gary E",
+"Phoenix, Brian R",
+"Gomez, Maureen",
+"Angeles, Emily M",
+"Moore, Timothy J",
+"Hunt, Matthias B",
+"Ige, Olukayode",
+"Morris, Christopher H",
+"Burkett, Erick A",
+"Cole, Stacy L",
+"Singh, Anna 'Chrissy' C",
+"Pedersen, Alyssa R",
+"Knop, Megan N",
+"Hunter, Angela T",
+"Kwabi, Kweku O",
+"Stutzman, Laurie A",
+"Gray, Jennifer S",
+"Watson, Erica M"]
 
    var random = foodArray[Math.floor(Math.random() * foodArray.length)];
    document.getElementById("message").innerHTML = random;
